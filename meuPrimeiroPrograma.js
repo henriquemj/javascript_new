04636abcba7aca826b80103c3a802c1e793b7e8a
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-03-31 21:29:30 -0300

    Operadores aritméticos

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu primeiro Programa em JS");
+
+const idade =26;
+
+console.log(idade);
+console.log(idade+5);
+console.log(idade-5);
+console.log(idade/5);
+console.log(idade*5);
+
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
