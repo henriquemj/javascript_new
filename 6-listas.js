74ac7e2f3b3fe6e6138d1b847e4a7601fe59d237
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-03-31 23:11:18 -0300

    atribuição de variáveis

---
+++ 6-listas.js
@@ -0,0 +1,19 @@
+console.log(`Trabalhando com listas`);
+// const salvador = 
+// const saoPaulo =
+// const rioDeJaneiro =
+// Use o comendo " CTRL + K + C " para marca todos os comentários 
+// Use o comendo " CTRL + K + U " para desmarca todos os comentários 
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+//Podemos usar o "push" para acrescentar um item a nossa lista
+listaDeDestinos.push(`Curitiba`)
+console.log("Destinos possíveis");
+//console.log(salvador, saoPaulo, rioDeJaneiro) >> usar dessa forma seria muito mais trabalhoso.
+
+console.log(listaDeDestinos);
