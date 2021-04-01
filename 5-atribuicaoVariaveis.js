e202b961eefbcc804a7816d42800cf8820e72bb0
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-03-31 22:18:08 -0300

    Const e let

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 26;
+const primeiroNome = "Henrique";
+const sobrenome = "Madureira";
+let contador = 0
+
+//console.log(nome + "" + sobrenome)  >> Concatenação
+console.log(primeiroNome, sobrenome)
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+let contador = 0
+contador = contador + 1
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto =2;
