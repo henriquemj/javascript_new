dcfed4e66f6067291e75550b2783638af2a112b3
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-02 11:27:22 -0300

    condicionais JS

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -3,28 +3,46 @@ const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
     `Rio de Janeiro`
-);
+    );
+    
+    const idadeComprador = 18;
+    const estaAcompanhada = true;
+    const temPassagemComprada = true;
+    
+    console.log("Destinos possíveis");
+    console.log(listaDeDestinos);
+    
+    // if(idadeComprador >= 18){
+    //     console.log("Comprador maior de idade");
+    //     listaDeDestinos.splice(1,1); //removendo item
+    // }else if(estaAcompanhada == true){
+    //     console.log("Comprador está acompanhado");
+    //     listaDeDestinos.splice(1,1); //removendo item
+    // }else{
+    //     console.log("Não é maior de Idade e não posso vender");
+    // }
+    
+    if(
+        idadeComprador >= 18 || estaAcompanhada == true
+        ){
+            listaDeDestinos.splice(2,1); //removendo item
+        } else {
+            console.log("Não é maior de Idade e não posso vender");
+        }
 
-const idadeComprador = 17;
-const estaAcompanhada = true;
-
-console.log("Destinos possíveis");
-console.log(listaDeDestinos);
-
-if(idadeComprador >= 18){
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1); //removendo item
-}else if(estaAcompanhada == true){
-    console.log("Comprador está acompanhado");
-    listaDeDestinos.splice(1,1); //removendo item
-}else{
-    console.log("Não é maior de Idade e não posso vender");
-}
-
-console.log(listaDeDestinos);
-
-// console.log(idadeComprador > 18); //maior que
-// console.log(idadeComprador < 18); //menor que
-// console.log(idadeComprador <= 18); //menor ou igual a
-// console.log(idadeComprador >= 18); //maior ou igual a
-// console.log(idadeComprador == 18); //igual
+        console.log ("Embarque: \n")
+        if(idadeComprador >= 18 && temPassagemComprada){
+            console.log("Boa viagem");
+        }else{
+            console.log("Você não pode embarcar");
+        }
+        
+        
+        console.log(listaDeDestinos);
+        
+        // console.log(idadeComprador > 18); //maior que
+        // console.log(idadeComprador < 18); //menor que
+        // console.log(idadeComprador <= 18); //menor ou igual a
+        // console.log(idadeComprador >= 18); //maior ou igual a
+        // console.log(idadeComprador == 18); //igual
+        
