77c6b41acba67114d747c21c1c4f1a52ec5509ee
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-02 21:19:00 -0300

    loops/ break

--- 8-loops.js
+++ 8-loops.js
@@ -16,11 +16,29 @@ const listaDeDestinos = new Array(
     const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
     let contador = 0;
+    let destinoExiste = false;
     while(contador<3){
+
         if(listaDeDestinos[contador] == destino){
-            console.log("Destino existe")
-        }else{
-            console.log("Destino não existe");
+            destinoExiste = true;
+            break;
         }
         contador += 1;
     }
+
+    console.log("Destino existe:" , destinoExiste);
+
+    if(podeComprar && destinoExiste){
+        console.log("Boa Viagem");
+    }else{
+        console.log("Desculpe tivemos um erro!");
+    }
+
+
+    for(let i = 0; i < 3 ; i++){
+        if(listaDeDestinos[i] == destino){
+            destinoExiste = true;
+        }
+    }
+
+    //Geralmente é utilizado a variavel "i" quando esta usando o FOR, para "incrementar" 
